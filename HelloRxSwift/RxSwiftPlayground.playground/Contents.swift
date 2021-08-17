9cfec6a61cf2e2f7222e5c71823ae4f02d1b273e
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-08-17 22:27:43 +0900

    Save the code

--- HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -3,13 +3,5 @@ import RxSwift
 import RxCocoa
 
 let disposeBag = DisposeBag()
-let relay = BehaviorRelay(value: "Initial Value")
 
-relay.accept("a")
-relay.asObservable()
-    .subscribe {
-        print($0)
-    }
-
-relay.accept("Hello World")
-relay.accept("s")
+let su
