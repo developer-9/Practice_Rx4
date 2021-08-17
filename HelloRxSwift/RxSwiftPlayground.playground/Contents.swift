f836ca0b691ec8850b3faeeb62c8d9fceac47132
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-08-17 22:55:36 +0900

    Save the code

--- HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RxSwiftPlayground.playground/Contents.swift
@@ -4,13 +4,13 @@ import RxCocoa
 
 let disposeBag = DisposeBag()
 
-let subject = PublishSubject<String>()
+let subject = BehaviorSubject(value: "Initial value")
 
-subject.onNext("Issue 1")
+subject.onNext("Last issue")
 
 subject.subscribe { event in
     print(event)
 }
 
-subject.onNext("Issue 2")
-subject.onNext("Issue 3")
+subject.onNext("Issue 1")
+
